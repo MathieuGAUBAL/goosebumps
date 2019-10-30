75ddaa73ac317b28df9c7e2be28c5f5fb86827f4
Louis Dupas <louisdupas@net-c.com>
2019-10-30 16:40:03 +0100

    ajout logo et couleurs navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import "./navbar.css";
+import logo from './logo.jpg'
 
 let Navbar = () =>
             <React.Fragment>
                 <nav className="header">
-                    <a href="index.html" className="brandlogo"></a>
-                    <a className="brand">GOOSEBUMPS</a>
+                    <a href="index.html" className="brandlogo"><img src={logo} alt="logo" width="45px"/></a>
+                    <a href="index.html" className="brand">GOOSEBUMPS</a>
                     <input className="menu-btn" type="checkbox" id="menu-btn" />
                     <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
                     <ul className="menu">
-                        <li><a href="#choose">HOME</a></li>
-                        <li><a href="#surprise">Quizz</a></li>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="quizz.html">QUIZZ</a></li>
                     </ul>
                 </nav>
             </React.Fragment>
